65566611bff9d5935db0ef495d37f3e55d862db2
Julius Lee <7julius.lee@gmail.com>
2021-07-24 15:08:18 -0700

    setup successful postgres connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+require('dotenv').config({ debug: true });
+
 const express = require('express');
 const morgan = require('morgan');
+const sequelize = require('./db/config');
 
 const app = express();
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => console.log(err));
 
 process.env.NODE_ENV == 'dev' && app.use(morgan('tiny'));
 
@@ -9,6 +19,6 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-const port = 3003;
+const port = process.env.PORT;
 
 app.listen(port, console.log(`Server Running on port ${port}`));
