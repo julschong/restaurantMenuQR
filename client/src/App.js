fa15ff35f7c59fa2e8e1746967dc3886f3c79181
Julius <71372051+julschong@users.noreply.github.com>
2021-08-04 01:40:21 -0700

    init frontend added chakra dep

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as React from 'react';
+// 1. import `ChakraProvider` component
+import { Button, ChakraProvider } from '@chakra-ui/react';
+function App({ Component }) {
+    // 2. Use at the root of your app
+    return (
+        <ChakraProvider>
+            <Button colorScheme="blue">Button</Button>
+        </ChakraProvider>
+    );
 }
-
 export default App;
