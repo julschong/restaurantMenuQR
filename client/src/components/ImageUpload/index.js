8415b82d861eafd1b4697ae62b5092553f14c20e
Julius Lee <7julius.lee@gmail.com>
2021-08-07 17:56:47 -0700

    trying upload image packages

---
+++ client/src/components/ImageUpload/index.js
@@ -0,0 +1,33 @@
+import { Image } from '@chakra-ui/image';
+import { useState } from 'react';
+import ImageUploader from 'react-images-upload';
+
+const ImageUpload = () => {
+    const [pictures, setPictures] = useState([]);
+    const [dataURL, setURL] = useState([]);
+
+    const onDrop = (pictureFiles, pictureDataURLs) => {
+        setPictures(pictureFiles);
+        setURL(pictureDataURLs);
+    };
+
+    console.log(pictures);
+    console.log(dataURL);
+
+    return (
+        <>
+            <ImageUploader
+                withIcon={true}
+                buttonText="Choose images"
+                onChange={onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif', '.jfif']}
+                maxFileSize={5242880}
+            />
+            {dataURL.map((el, i) => (
+                <Image src={el} key={`${pictures[i].name}${i}`} />
+            ))}
+        </>
+    );
+};
+
+export default ImageUpload;
--- client/src/pages/Edit/index.js
+++ client/src/pages/Edit/index.js
@@ -8,6 +8,7 @@ import { VscLoading } from 'react-icons/vsc';
 import './edit.scss';
 import { Flex } from '@chakra-ui/layout';
 import { PATHS } from './../../components/Links/index';
+import ImageUpload from './../../components/ImageUpload/index';
 
 const Edit = () => {
     const { id } = useParams();
@@ -38,6 +39,7 @@ const Edit = () => {
                 <Image />
                 <Flex direction="column">
                     <h2 className="title">{restaurant.name}</h2>
+                    <ImageUpload />
                 </Flex>
             </Flex>
         </Container>
